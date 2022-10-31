c395598f6e9e5351467d08d65935bd25a2112b89
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-10-31 10:42:17 +0000

    Edits hechos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* ORIGINAL CODE
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +24,131 @@ function App() {
 }
 
 export default App;
+END OF ORIGINAL CODE */ 
+/* MY CODE
+import React, { useState } from "react";
+import { PageLayout } from "./components/PageLayout";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import Button from "react-bootstrap/Button";
+import { ProfileData } from "./components/ProfileData";
+import { callMsGraph } from "./graph";
+
+function ProfileContent() {
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
+
+  const name = accounts[0] && accounts[0].name;
+
+  function RequestProfileData() {
+      const request = {
+          ...loginRequest,
+          account: accounts[0]
+      };
+
+      // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+      instance.acquireTokenSilent(request).then((response) => {
+          callMsGraph(response.accessToken).then(response => setGraphData(response));
+      }).catch((e) => {
+          instance.acquireTokenPopup(request).then((response) => {
+              callMsGraph(response.accessToken).then(response => setGraphData(response));
+          });
+      });
+  }
+
+  return (
+      <>
+          <h5 className="card-title">Welcome {name}</h5>
+          {graphData ? 
+              <ProfileData graphData={graphData} />
+              :
+              <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
+          }
+      </>
+  );
+};
+
+function App() {
+  return (
+      <PageLayout>
+          <AuthenticatedTemplate>
+              <ProfileContent />
+          </AuthenticatedTemplate>
+          <UnauthenticatedTemplate>
+              <p>You are not signed in! Please sign in.</p>
+          </UnauthenticatedTemplate>
+      </PageLayout>
+  );
+}
+
+export default App;
+END OF MY CODE */
+
+import React, { useState } from 'react';
+import './App.css';
+import { PageLayout } from './components/PageLayout';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import Button from 'react-bootstrap/Button';
+import { loginRequest } from './authConfig';
+import { callMsGraph } from './graph';
+import { ProfileData } from './components/ProfileData';
+
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance
+            .acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0],
+            })
+            .then((response) => {
+                callMsGraph(response.accessToken).then((response) => setGraphData(response));
+            });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+            {graphData ? (
+                <ProfileData graphData={graphData} />
+            ) : (
+                <Button variant="secondary" onClick={RequestProfileData}>
+                    Request Profile Information
+                </Button>
+            )}
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to see your profile information.</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
+
